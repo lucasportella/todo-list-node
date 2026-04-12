66ae4ba913e4d3f0e500284f16a983d90348ffff
lucas <lucasportellaagu@gmail.com>
2026-04-12 12:28:08 -0300

    fix: error middleware hardcoded msg

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -13,7 +13,7 @@ export class AppError extends Error {
 
 export function errorHandler(err: unknown, req: Request, res: Response, next: NextFunction) {
   if (err instanceof AppError) {
-    res.status(err.statusCode).json({ message: "err.message" });
+    res.status(err.statusCode).json({ message: err.message });
   }
   if (err instanceof Error && 'code' in err && err.code === 'ER_DUP_ENTRY') {
     res.status(StatusCodes.CONFLICT).json({ message: 'Email already in use' })
